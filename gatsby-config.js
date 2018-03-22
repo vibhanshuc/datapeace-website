ed32473356fb068599ecf315cd0222d39ecdf0fb
Vibhanshu <vibhanshu86@gmail.com>
2018-03-22 13:20:34 +0530

    WIP - CMS integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,5 +45,11 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 };
