20ea74b729c8d5f6319d45d686510c1681bed280
Vibhanshu <vibhanshu86@gmail.com>
2018-04-10 23:43:06 +0530

    first page complete with new design

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -10,179 +10,157 @@ import NaturalLanguageProcessingIcon from './Icons/NaturalLanguageProcessing.svg
 import ConversationalDialogueSystemsIcon from './Icons/ConversationalDialogueSystems.svg';
 import './skills.scss';
 
-const Skills = () => (
-  <div className="Skills">
-    <div className="flex-center flex-col">
-      <h2 className="Skills-heading">What We Do</h2>
+const SkillItem = ({ icon, title, description }) => (
+  <div className="Skills-grid__item flex flex-50">
+    <div className="Item-icon">
+      <img src={icon} />
     </div>
-    <div className="flex flex-col flex-center Skills-description">
-      <p>
-        We deliver the benefits of data for you through cutting edge
-        technologies
-      </p>
-      <p>and we prepare you for your journey to a Cloud ecosystem</p>
-    </div>
-    <div className="Skills-grid flex flex-wrap">
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={CloudMigrationIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          <div className="Item-title">
-            <h4>Cloud Migration</h4>
-          </div>
-          <p className="Item-description">
-            We manage your journey to single cloud/ multi-cloud/ hybrid cloud
-            environments. We provide devops services, cloud migration services,
-            cloud application cloud native custom development and cloud
-            infrastructure transformation services.
-          </p>
-        </div>
+    <div className="flex flex-col flex-center">
+      <div className="Item-title">
+        <h4>{title}</h4>
       </div>
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={CloudManagementIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          <div className="Item-title">
-            <h4>Cloud Management</h4>
-          </div>
-          <p className="Item-description">
-            Once on the cloud we ensure that you achieve the desired outcomes
-            with your data and applications parked there. We provision and
-            maintain cloud- based architecture solutions employing
-            micro-services, distributed database systems, load-balancing and
-            caching.
-          </p>
-        </div>
-      </div>
-
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={HighPerformanceComputingIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          {' '}
-          <div className="Item-title">
-            <h4>High Performance Computing</h4>
-          </div>
-          <p className="Item-description">
-            The cloud is useless if it is not equip you to do more with your
-            data. We build for you quality, well-documented, ouctome focused,
-            modularized software across processors, workstations and cloud
-            computing devices running the latest big data tool components.
-            Porting existing in-house or cloud based applications to Kubernetes
-            to scale them and reduce the running and maintenance cost.
-          </p>
-        </div>
-      </div>
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={DataLakeImplementationIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          {' '}
-          <div className="Item-title">
-            <h4>Data Lake Implementation</h4>
-          </div>
-          <p className="Item-description">
-            Don’t lose your data to want of better warehousing solutions. We are
-            experts in Data Lake implementations. This modern version of data
-            warehousing powered on clouds like AWS, Azure, Google Cloud will
-            ensure that ALL your data is available to you for posterity.
-            <span>
-              We are ready to cater to a world that will reside on multi-cloud
-              ecosystems. We are here to make the cloud a reality for ALL
-              businesses
-            </span>
-          </p>
-        </div>
-      </div>
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={BigDataAnalyticsIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          {' '}
-          <div className="Item-title">
-            <h4>Big Data Analytics</h4>
-          </div>
-          <p className="Item-description">
-            Gain insights from your data. Uncover data, forecast future events
-            and take steps based on sound statistical reasoning to maximize your
-            revenue and reduce costs.
-          </p>
-        </div>
-      </div>
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={MachineLearningIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          {' '}
-          <div className="Item-title">
-            <h4>Machine Learning</h4>
-          </div>
-          <p className="Item-description">
-            Use state-of- art machine learning techniques for classification,
-            regression and unsupervised learning problems. Pipelined models then
-            get deployed on software stacks.
-          </p>
-        </div>
-      </div>
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={RecommenderSystemsIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          <div className="Item-title">
-            <h4>Recommender Systems</h4>
-          </div>
-          <p className="Item-description">
-            Build scalable recommender systems combining collaborative
-            filtering, content based filtering models and graph-based models to
-            deliver domain specific recommendations.
-          </p>
-        </div>
-      </div>
-      <div className="Skills-grid__item flex flex-50">
-        <div className="Item-icon">
-          <img src={NaturalLanguageProcessingIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          {' '}
-          <div className="Item-title">
-            <h4>Natural Language Processing</h4>
-          </div>
-          <p className="Item-description">
-            Extract every ounce of information from textual and social media
-            data. Deploy mamed entity detection, keyword annotation, dependency
-            parsing, topic modeling to understand your data better.
-          </p>
-        </div>
-      </div>
-      <div className="Skills-grid__item flex flex-100">
-        <div className="Item-icon">
-          <img src={ConversationalDialogueSystemsIcon} />
-        </div>
-        <div className="flex flex-col flex-center">
-          {' '}
-          <div className="Item-title">
-            <h4>Conversational Dialogue Systems</h4>
-          </div>
-          <p className="Item-description">
-            We build chatbots that provide deep first touch-points for your
-            customers. Our bots embedded with deep understanding of your
-            industry and organization focus on providing near human interface
-            experience to your users. When we think of Machine Learning and AI
-            we think of complexities and prohibitive costs. Data Peace is bent
-            on making AI accessible to all. More so to small businesses who will
-            benefit most from it.
-          </p>
-        </div>
-      </div>
+      <p className="Item-description">{description}</p>
     </div>
   </div>
 );
+
+const items = [
+  {
+    title: 'Cloud Migration',
+    icon: CloudMigrationIcon,
+    description:
+      'We manage your journey to single cloud/ multi-cloud/ hybrid cloud ' +
+      'environments. We provide devops services, cloud migration services, ' +
+      'cloud application cloud native custom development and cloud ' +
+      'infrastructure transformation services.',
+  },
+  {
+    title: 'Cloud Management',
+    icon: CloudManagementIcon,
+    description:
+      ' Once on the cloud we ensure that you achieve the desired outcomes\n' +
+      'with your data and applications parked there. We provision and\n' +
+      'maintain cloud- based architecture solutions employing\n' +
+      'micro-services, distributed database systems, load-balancing and\n' +
+      'caching.',
+  },
+  {
+    title: 'High Performance Computing',
+    icon: HighPerformanceComputingIcon,
+    description:
+      'The cloud is useless if it is not equip you to do more with your\n' +
+      '            data. We build for you quality, well-documented, ouctome focused,\n' +
+      '            modularized software across processors, workstations and cloud\n' +
+      '            computing devices running the latest big data tool components.\n' +
+      '            Porting existing in-house or cloud based applications to Kubernetes\n' +
+      '            to scale them and reduce the running and maintenance cost.',
+  },
+  {
+    title: 'Data Lake Implementation',
+    icon: DataLakeImplementationIcon,
+    description:
+      'Don’t lose your data to want of better warehousing solutions. We are\n' +
+      '            experts in Data Lake implementations. This modern version of data\n' +
+      '            warehousing powered on clouds like AWS, Azure, Google Cloud will\n' +
+      '            ensure that ALL your data is available to you for posterity.\n' +
+      '            We are ready to cater to a world that will reside on multi-cloud\n' +
+      '            ecosystems. We are here to make the cloud a reality for ALL\n' +
+      '            businesses.',
+  },
+  {
+    title: 'Big Data Analytics',
+    icon: BigDataAnalyticsIcon,
+    description:
+      'Gain insights from your data. Uncover data, forecast future events\n' +
+      '            and take steps based on sound statistical reasoning to maximize your\n' +
+      '            revenue and reduce costs.',
+  },
+  {
+    title: 'Machine Learning',
+    icon: MachineLearningIcon,
+    description:
+      'Use state-of- art machine learning techniques for classification,\n' +
+      '            regression and unsupervised learning problems. Pipelined models then\n' +
+      '            get deployed on software stacks.',
+  },
+  {
+    title: 'Recommender Systems',
+    icon: RecommenderSystemsIcon,
+    description:
+      'Build scalable recommender systems combining collaborative\n' +
+      '            filtering, content based filtering models and graph-based models to\n' +
+      '            deliver domain specific recommendations.',
+  },
+  {
+    title: 'Natural Language Processing',
+    icon: NaturalLanguageProcessingIcon,
+    description:
+      'Extract every ounce of information from textual and social media\n' +
+      '            data. Deploy mamed entity detection, keyword annotation, dependency\n' +
+      '            parsing, topic modeling to understand your data better.',
+  },
+  {
+    title: 'Conversational Dialogue Systems',
+    icon: ConversationalDialogueSystemsIcon,
+    description:
+      'We build chatbots that provide deep first touch-points for your\n' +
+      '            customers. Our bots embedded with deep understanding of your\n' +
+      '            industry and organization focus on providing near human interface\n' +
+      '            experience to your users. When we think of Machine Learning and AI\n' +
+      '            we think of complexities and prohibitive costs. Data Peace is bent\n' +
+      '            on making AI accessible to all. More so to small businesses who will\n' +
+      '            benefit most from it.',
+  },
+];
+
+class Skills extends React.Component {
+  state = {
+    activeIndex: 0,
+  };
+
+  handleLinkClick = (index) => {
+    this.setState({
+      activeIndex: index,
+    });
+  };
+
+  render() {
+    const activeItem = items[this.state.activeIndex];
+    return (
+      <div className="Skills">
+        <div className="flex-center flex-col">
+          <h2 className="Skills-heading">What We Do</h2>
+        </div>
+        <div className="flex flex-col flex-center Skills-description">
+          <p>
+            We deliver the benefits of data for you through cutting edge
+            technologies
+          </p>
+          <p>and we prepare you for your journey to a Cloud ecosystem</p>
+        </div>
+        <div className="Skills-grid flex flex-wrap">
+          <div className="flex-40">
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className={`Skills-link ${index === this.state.activeIndex ? 'active' : ''}`}
+                onClick={() => this.handleLinkClick(index)}
+              >
+                {item.title}
+              </div>
+            ))}
+          </div>
+          <div className="flex-60">
+            <SkillItem
+              title={activeItem.title}
+              icon={activeItem.icon}
+              description={activeItem.description}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Skills;
