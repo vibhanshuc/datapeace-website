b6c06599b2d2a583d6c888c78fd0fba31acd8d64
Vibhanshu <vibhanshu86@gmail.com>
2018-04-19 22:15:38 +0530

    removes author image

--- src/components/Blog/BlogHeader.js
+++ src/components/Blog/BlogHeader.js
@@ -3,7 +3,7 @@ import './index.scss';
 
 const BlogHeader = ({ date, title }) => (
   <div className="Blog-header flex">
-    <img src="/img/ravi.jpg" alt="avatar" />
+    {/*<img src="/img/ravi.jpg" alt="avatar" />*/}
     <div className="BlogsList-item-header">
       <div className="flex">
         <h5>Ravi Naval</h5>
