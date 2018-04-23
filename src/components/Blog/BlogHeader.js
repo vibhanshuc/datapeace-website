823917cd8f945ad69d30ac1b338030554fc58209
Vibhanshu <vibhanshu86@gmail.com>
2018-04-23 19:38:51 +0530

    few fixes in style

--- src/components/Blog/BlogHeader.js
+++ src/components/Blog/BlogHeader.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import Link from 'gatsby-link';
 import './index.scss';
 
-const BlogHeader = ({ date, title }) => (
+const BlogHeader = ({ date, title, link }) => (
   <div className="Blog-header flex">
-    {/*<img src="/img/ravi.jpg" alt="avatar" />*/}
+    {/* <img src="/img/ravi.jpg" alt="avatar" /> */}
     <div className="BlogsList-item-header">
+      <h1><Link
+        to={link}
+      >
+        {title}
+
+          </Link>
+      </h1>
       <div className="flex">
         <h5>Ravi Naval</h5>
         <h5>{date}</h5>
       </div>
-      <h1>
-        {title}{' '}
-      </h1>
     </div>
   </div>
 );
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import FaDoubleAngle from 'react-icons/lib/fa/angle-double-right';
 import Header from '../../components/Header';
 import BlogHeader from '../../components/Blog/BlogHeader';
 import './index.scss';
@@ -14,15 +15,17 @@ export default ({ data }) => (
     <div className="BlogsList">
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div className="BlogsList-item" key={node.id}>
-          <BlogHeader title={node.frontmatter.title} date={node.frontmatter.date} />
-          <p>{node.frontmatter.description}</p>
-          <Link
-            className="btn btn-primary"
-            to={`/blog${node.fields.slug}`}
-            css={{ textDecoration: 'none', color: 'inherit' }}
-          >
-              Read More
-          </Link>
+          <BlogHeader title={node.frontmatter.title} date={node.frontmatter.date} link={`/blog${node.fields.slug}`} />
+          <p>{node.frontmatter.description}
+            <Link
+                className="ReadMore"
+              to={`/blog${node.fields.slug}`}
+              css={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              Read More <FaDoubleAngle />
+            </Link>
+          </p>
+
         </div>
     ))}
     </div>
