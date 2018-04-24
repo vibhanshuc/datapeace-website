87afc27f75966eaae573cfb973a0f44b4558ee13
Vibhanshu <vibhanshu86@gmail.com>
2018-04-24 17:18:46 +0530

    adds favicon

---
+++ src/html.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import favicon from './favicon.jpg';
+
+let stylesStr;
+if (process.env.NODE_ENV === 'production') {
+  try {
+    stylesStr = require('!raw-loader!../public/styles.css');
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const propTypes = {
+  headComponents: PropTypes.node.isRequired,
+  body: PropTypes.node.isRequired,
+  preBodyComponents: PropTypes.node.isRequired,
+  postBodyComponents: PropTypes.node.isRequired,
+  bodyAttributes: PropTypes.node.isRequired,
+  htmlAttributes: PropTypes.string,
+};
+
+
+class HTML extends Component {
+  render() {
+    let css;
+    if (process.env.NODE_ENV === 'production') {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      );
+    }
+    return (
+      <html lang="en-US" {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link rel="icon" type="image/png" href={favicon} />
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key="body"
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
+}
+
+HTML.propTypes = propTypes;
+
+module.exports = HTML;
