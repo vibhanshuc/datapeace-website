d4b7c240a3b9ba9a567550a61b48b4c4a19d10e1
Vibhanshu Chaturvedi <vibhanshu86@gmail.com>
2019-01-30 20:07:06 +0530

    remove list of clients

--- src/components/Clients/index.js
+++ src/components/Clients/index.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import Heading from '../shared/Heading';
 import './clients.scss';
-import Astro from './astro.svg';
 
-const items = [
-  {
-    title: 'Blazeclan',
-    description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' +
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    imageURL:
-      'https://www.blazeclan.com/wp-content/uploads/2017/01/logo-1-1.png',
-  },
-  {
-    title: 'RateGain',
-    description:
-      'For Travel companies the disruption is not just from new entrants or nimble competition, ' +
-      'but also from the connected traveler who might take his business elsewhere',
-    imageURL: 'https://rategain.com/wp-content/uploads/2017/01/rlv200x60.png',
-  },
-  {
-    title: 'Astro',
-    description:
-        'For Travel companies the disruption is not just from new entrants or nimble competition, ' +
-        'but also from the connected traveler who might take his business elsewhere',
-    imageURL: Astro,
-  },
-];
+const items = [];
 
 const Client = ({ title, imageURL, description }) => (
   <div className="Client flex flex-col flex-center">
